2ef78b0f58ac62fd5a7555da2f88febc7b4df9aa
HARISRUJAN <65223347+HARISRUJAN@users.noreply.github.com>
2021-06-22 15:14:02 +0530

    DAY_02
    Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
    
    Declare  variables: one of type int, one of type double, and one of type String.
    Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
    Use the  operator to perform the following operations:
    Print the sum of  plus your int variable on a new line.
    Print the sum of  plus your double variable to a scale of one decimal place on a new line.
    Concatenate  with the string you read as input and print the result on a new line.

---
+++ DAY_02.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+int_1=int(input())
+# Read and save an integer, double, and String to your variables.
+dou_1=float(input())
+# Print the sum of both integer variables on a new line.
+str_1=input()
+# Print the sum of the double variables on a new line.
+print(i+int_1)
+print(d+dou_1)
+print(s+str_1)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
